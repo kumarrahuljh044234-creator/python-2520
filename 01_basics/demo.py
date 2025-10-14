7722b891aa338b4e78ea4111a1e6b7c5cb1b22c7
kumarrahul4944 <kumarrahuljh04@gmail.com>
2025-10-14 16:42:11 +0530

    Syntax

---
+++ 01_basics/demo.py
@@ -0,0 +1,15 @@
+# Means Commenet ,Python will skip execution this line
+# we used script mode to execute below code
+
+#Syntax (rules)
+print(9)
+#print(good morning)#yntaxError: invalid syntax. Perhaps you forgot a comma?
+#good morning is text data 
+#In python rule is ,If you have text data then we need to enclose that in ""or''
+
+print("good morning")
+print('good morning')
+
+#print(hello)# Name 'hello'is not defined.did you mean :'help'?
+hello="hello hi"
+print(hello)
