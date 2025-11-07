c1f46e3745868aae34d70fcd532317ea2ffccb4e
kumarrahul4944 <kumarrahuljh04@gmail.com>
2025-11-07 10:55:57 +0530

    conditionals-ternary,elif,match-case

--- 05_control_statment/demo.py
+++ 05_control_statment/demo.py
@@ -63,3 +63,38 @@ if age >= 18:#TypeError: '>=' not supported between instances of 'str' and 'int'
     print("you can vote")
 else:
     print("you can not vote")
+
+number = int(input("enter your number:"))
+print(number+2)
+
+#above using ternary operators
+# variables = value_if_true if condition else value_if_false
+age = int(input("Enter your age:"))
+status = "you can vote"if age >=18 else "you cannot vote"
+print(status)
+
+
+#elif ladder
+marks = int(input("Enter your marks:"))
+if marks >=90:
+    print("A")
+elif marks >=80:
+    print("B")
+elif marks >=60:
+    print("C")
+elif marks >=35:
+    print("D")
+else:
+    print("failed")
+
+#match choice
+choice = int(input("enter your choice:"))
+match choice:
+    case 1:
+        print("python")
+    case 2:
+        print("java")
+    case 3:
+        print("c#")
+    case _:
+        print("select bt (1-3)")
