ce02f5ee8c2feb5f39fe74e6e94c024750bc4e1f
kumarrahul4944 <kumarrahuljh04@gmail.com>
2025-11-09 21:51:46 +0530

    nested conditionals & loops

--- 05_control_statment/nested.py
+++ 05_control_statment/nested.py
@@ -17,3 +17,23 @@ for row in range(1, 8):           # Outer loop (1 to 5)
     for column in range(1, 4):       # Inner loop (1 to 5)
         print(f"{row} * {column} = {row*column}")
     print("-------------------")     # Blank line after each table
+
+#Outer loop (row) → decides which table (1 to 5).
+#Inner loop (column) → goes through numbers (1 to 5).
+#Nested loops together → produce all combinations of multiplication.
+#Blank line (print()) → separates each table for clarity.
+
+
+#While with Nested 
+ 
+#while(condition):
+#statment
+
+row =1 
+while row <6:
+    column =1
+    while column<6:
+        print(f"{row}*{column}={row*column}")
+        column+=1
+    print("_______________")
+    row+=1
